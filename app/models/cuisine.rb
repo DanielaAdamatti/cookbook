d4f8aa2159b78dc5fa021fc7a0f02e7c7e56b236
DanielaAdamatti <dani.adamatti@gmail.com>
2019-09-06 14:59:26 -0300

    admin register cuisine

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,3 +1,5 @@
 class Cuisine < ApplicationRecord
   has_many :recipes
+  validates :name, presence: { message: "Você deve informar o nome da Cozinha" }
+  validates :name, uniqueness: { case_sensitive: false, message: "Cozinha já cadastrada"  }
 end
--- spec/features/admin_register_cuisine_spec.rb
+++ spec/features/admin_register_cuisine_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'admin register recipe type' do
+feature 'admin register cuisine' do
   scenario 'successfully' do
     user = User.create!(email:"admin@email.com", password:"123456",admin: true)
 
@@ -17,5 +17,58 @@ feature 'admin register recipe type' do
 
     #expectativa do usuário após a ação
     expect(page).to have_css('h1', text: 'Árabe')
+  end
+
+  scenario 'and must fill in name' do
+    user = User.create!(email:"admin@email.com", password:"123456",admin: true)
+
+    visit root_path
+    click_on "Entrar"
+    fill_in "E-mail", with: 'admin@email.com'
+    fill_in "Senha", with: '123456'
+    click_on 'Enviar'
+
+    click_on "Cadastrar Cozinha"
+    fill_in "Nome", with: ''
+    click_on 'Enviar'
+
+    #expectativa do usuário após a ação
+    expect(page).to have_content('Você deve informar o nome da Cozinha')
+  end
+
+  scenario 'and checks uniqueness' do
+    user = User.create!(email:"admin@email.com", password:"123456",admin: true)
+
+    visit root_path
+    click_on "Entrar"
+    fill_in "E-mail", with: 'admin@email.com'
+    fill_in "Senha", with: '123456'
+    click_on 'Enviar'
+
+    click_on "Cadastrar Cozinha"
+    fill_in "Nome", with: 'Arabe'
+    click_on 'Enviar'
+
+    visit root_path
+    click_on "Cadastrar Cozinha"
+    fill_in "Nome", with: 'Arabe'
+    click_on 'Enviar'
+
+    #expectativa do usuário após a ação
+    expect(page).to have_content('Cozinha já cadastrada')
+  end
+
+  scenario 'and must be loged in as admin to register cuisine' do
+    user = User.create!(email:"admin@email.com", password:"123456",admin: false)
+
+    #simula ação do usuário
+    visit root_path
+    click_on "Entrar"
+    fill_in "E-mail", with: 'admin@email.com'
+    fill_in "Senha", with: '123456'
+    click_on 'Enviar'
+
+    #expectativa do usuário após a ação
+    expect(page).not_to have_link('Cadastrar Cozinha')
   end
 end
